7eade79521d544075439091ef5847986f0d172df
tony4win <d00246497@student.dkit.ie>
2024-11-06 04:45:26 +0000

    Remove update from kind setup

--- kind_setup.sh
+++ kind_setup.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
 ### Setup Kind ###
 
-sudo apt update -y 
-sudo apt upgrade -y
-
 # Kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
