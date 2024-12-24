700c30b8706f35389e114546ca45202cdb97f4ce
tonymagnum <tonyh47@protonmail.com>
2024-12-24 02:38:37 +0000

    Update runner_setup.sh

--- runner_setup.sh
+++ runner_setup.sh
@@ -2,11 +2,7 @@
 ### Gitlab Runner Setup ###
 
 # Download Package
-curl -LJO "https://s3.dualstack.us-east-1.amazonaws.com/gitlab-runner-downloads/latest/deb/gitlab-runner_amd64.deb"
+curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | sudo bash
 
-# Install Package & Any Dependancies
-sudo dpkg -i gitlab-runner_amd64.deb || sudo apt-get install -y -f
-
-# Verify Setup
-gitlab-runner --version
-sudo gitlab-runner status
+# Install
+sudo apt install gitlab-runner
